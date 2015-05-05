6c464239672253999df504133dcaafa3c5872415
Stéphane Marchesin <marcheu@chromium.org>
2015-05-05 14:26:08 -0700

    CHROMIUM: i965: disable hw contexts on gen6
    They don't seem to work, and cause regular GPU hangs, so
    let's disable them.
    
    BUG=chromium:288818
    TEST=by hand: (along with the kernel patch) run multiple flash videos with hardware decode, no GPU hang happens
    
    Change-Id: I0775c8f63ed1848d4948a7c1c80959f1753d063b
    Signed-off-by: Dominik Behr <dbehr@chromium.org>
    Signed-off-by: Prince Agyeman <prince.agyeman@intel.com>
    Signed-off-by: Dhinakaran Pandiyan <dhinakaran.pandiyan@intel.com>
    Signed-off-by: James Ausmus <james.ausmus@intel.com>
    Signed-off-by: Tomasz Figa <tfiga@chromium.org>

--- src/mesa/drivers/dri/i965/brw_context.c
+++ src/mesa/drivers/dri/i965/brw_context.c
@@ -969,7 +969,7 @@ brwCreateContext(gl_api api,
 
    intel_batchbuffer_init(brw);
 
-   if (brw->gen >= 6) {
+   if (brw->gen >= 7) {
       /* Create a new hardware context.  Using a hardware context means that
        * our GPU state will be saved/restored on context switch, allowing us
        * to assume that the GPU is in the same state we left it in.
@@ -979,11 +979,11 @@ brwCreateContext(gl_api api,
        */
       brw->hw_ctx = drm_intel_gem_context_create(brw->bufmgr);
 
-      if (!brw->hw_ctx) {
+/*      if (!brw->hw_ctx) {
          fprintf(stderr, "Gen6+ requires Kernel 3.6 or later.\n");
          intelDestroyContext(driContextPriv);
          return false;
-      }
+      }*/
    }
 
    if (brw_init_pipe_control(brw, devinfo)) {
