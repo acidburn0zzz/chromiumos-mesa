8e5a84bbfb55f703b6a376bf71defd088cfc7210
Gurchetan Singh <gurchetansingh@chromium.org>
2016-04-26 14:14:05 -0700

    CHROMIUM: i965: Integrate precise trig into configuration infrastructure
    With this change, to enable precise SIN and COS instructions
    on Intel hardware, one can put
    
    <option name="intel_precise_trig" value="true"/>
    
    in the proper drirc file.
    
    Change-Id: Idb6e6d3372267d4c6ddde2cd1c3fa6f13eaebe57

--- src/mesa/drivers/dri/i965/brw_context.c
+++ src/mesa/drivers/dri/i965/brw_context.c
@@ -769,6 +769,9 @@ brw_process_driconf_options(struct brw_context *brw)
 
    brw->precompile = driQueryOptionb(&brw->optionCache, "shader_precompile");
 
+   brw->intelScreen->compiler->precise_trig =
+      driQueryOptionb(&brw->optionCache, "intel_precise_trig");
+
    ctx->Const.ForceGLSLExtensionsWarn =
       driQueryOptionb(options, "force_glsl_extensions_warn");
 
